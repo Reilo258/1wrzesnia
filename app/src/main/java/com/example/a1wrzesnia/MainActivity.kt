c7d9f7d557fc967494929c63ab142c802a8d7c19
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-09-02 15:50:11 +0200

    Dodanie i przycisku

--- app/src/main/java/com/example/a1wrzesnia/MainActivity.kt
+++ app/src/main/java/com/example/a1wrzesnia/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.a1wrzesnia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
